daeee8ec5e204b513c8ceb3a451a49feeca41e88
Sumukh-star <51879944+Sumukh-star@users.noreply.github.com>
2021-11-19 23:16:42 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.mad_mini_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner myspinner =(Spinner) findViewById(R.id.spinner2);
+        Spinner myspinner1 =(Spinner) findViewById(R.id.spinner3);
+
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.names)
+
+        );
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner.setAdapter(myAdapter);
+
+
+
+
+
+
+
+
+        ArrayAdapter<String> myAdapter1 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.names1)
+
+        );
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner1.setAdapter(myAdapter1);
+
+
+
+
+
+
+    }
+
+    public void start(View v){
+        Spinner myspinner =(Spinner) findViewById(R.id.spinner2);
+        Spinner myspinner1 =(Spinner) findViewById(R.id.spinner3);
+
+        String cat = myspinner.getSelectedItem().toString();
+        String dif = myspinner1.getSelectedItem().toString();
+        Intent i = new Intent(this,apifetch.class);
+        Bundle b = new Bundle();
+        b.putString("cat" , cat);
+        b.putString("dif",dif);
+        i.putExtras(b);
+        startActivity(i);
+
+    }
+
+
+}
---
+++ apifetch.java
@@ -0,0 +1,220 @@
+package com.example.mad_mini_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class apifetch extends AppCompatActivity {
+    JSONArray res;
+    int counter =0;
+    JSONObject question;
+    EditText time;
+    CountDownTimer timer =new CountDownTimer(11000, 1000) {
+
+        public void onTick(long millisUntilFinished) {
+            time.setText(String.valueOf(millisUntilFinished/1000));
+            //here you can have your logic to set text to edittext
+        }
+
+        public void onFinish() {
+            //time.setText("done!");
+            Intent j=new Intent(apifetch.this,final_Activity.class);
+            Bundle b = new Bundle();
+            b.putString("txt","You lose. Your Score is "+String.valueOf(counter));
+            j.putExtras(b);
+            startActivity(j);
+        }
+
+    };//.start();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apifetch);
+        Bundle b = getIntent().getExtras();
+        String cat = b.getString("cat");
+
+        String dif = b.getString("dif");
+        TextView heading = findViewById(R.id.heading);
+        TextView questionplace = findViewById(R.id.question);
+        time=findViewById(R.id.time);
+        heading.setText("Catagory: "+cat);
+
+
+        String category="";
+
+        String difficulty="";
+
+
+        RequestQueue requestQueue;
+        if(cat.equals("General Knowledge") ){
+            category = "&category=9";
+        }
+        else if(cat.equals("Computer Science") ){
+            category ="&category=18";
+        }
+
+        if(dif.equals("Easy")){
+            difficulty = "&difficulty=easy";
+        }
+
+        else if(dif.equals("Medium")) {
+            difficulty = "&difficulty=medium";
+        }
+
+        else
+            difficulty ="&difficulty=hard";
+
+
+        String url = "https://opentdb.com/api.php?amount=10&type=boolean";  //35
+        if(category.length()>0)
+            url =url + category;
+        if(difficulty.length()>0)
+            url = url +difficulty;
+        requestQueue = Volley.newRequestQueue(this);
+
+
+
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                res = response.optJSONArray("results");
+
+                try {
+                     question = res.getJSONObject(0);
+
+                    String Question = question.optString("question").toString();
+                    Question =Question.replace("&quot;","\"\"");
+                    Question =Question.replace("&#039;","'");
+                    questionplace.setText(Question);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                //Toast.makeText(MainActivity.this, res.toString(), Toast.LENGTH_SHORT).show();
+                //check.append(res.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("myapp", "something went wrong");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+        timer.cancel();
+        timer.start();
+    }
+
+    public void True_clicked(View v) throws JSONException {
+        String answer = question.optString("correct_answer").toString();
+        if(answer.equals("True")){
+            TextView questionplace = findViewById(R.id.question);
+            Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show();
+            counter++;
+            if(counter==10){
+                //Intent j = new Intent(this,MainActivity.class);
+                //Toast.makeText(this, "you win", Toast.LENGTH_SHORT).show();
+                //startActivity(j);
+                timer.cancel();
+                Intent j = new Intent(this,final_Activity.class);
+
+                Bundle b = new Bundle();
+                //String message="You win. Your score is ";//+String.valueOf(counter);
+                b.putString("txt", "You won");
+                j.putExtras(b);
+                startActivity(j);
+            }else {
+                question = res.getJSONObject(counter);
+                String Question = question.optString("question").toString();
+                Question = Question.replace("&quot;", "\"\"");
+                Question = Question.replace("&#039;", "'");
+                questionplace.setText(Question);
+                timer.cancel();
+                timer.start();
+            }
+            
+        }
+        else{
+            //Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
+            timer.cancel();
+            Intent j = new Intent(this,final_Activity.class);
+            Bundle b = new Bundle();
+            b.putString("txt","You lose. Your Score is "+String.valueOf(counter));
+            //b.putString("finid","Better luck next time. Your score is "+String.valueOf(counter));
+            //Toast.makeText(this, "you lose, You're score = "+counter, Toast.LENGTH_SHORT).show();
+            j.putExtras(b);
+            startActivity(j);
+
+        }
+    }
+
+    public void False_clicked(View v) throws JSONException {
+        String answer = question.optString("correct_answer").toString();
+        if(answer.equals("False")){
+            TextView questionplace = findViewById(R.id.question);
+            Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show();
+            counter++;
+            if(counter==10){
+               // Intent j = new Intent(this,MainActivity.class);
+                //Toast.makeText(this, "you win", Toast.LENGTH_SHORT).show();
+                //startActivity(j);
+                timer.cancel();
+                Intent j = new Intent(this,final_Activity.class);
+                Bundle b = new Bundle();
+                //String message="You win. Your score is ";//+String.valueOf(counter);
+                b.putString("txt", "You won");
+                j.putExtras(b);
+                startActivity(j);
+            }else {
+                question = res.getJSONObject(counter);
+                String Question = question.optString("question").toString();
+                Question = Question.replace("&quot;", "\"\"");
+                Question = Question.replace("&#039;", "'");
+                questionplace.setText(Question);
+                timer.cancel();
+                timer.start();
+            }
+
+        }
+        else{
+            /*Toast.makeText(this, "you lose, You're score = "+counter, Toast.LENGTH_SHORT).show();
+            Intent j = new Intent(this,MainActivity.class);
+
+            startActivity(j);*/
+            timer.cancel();
+            Intent j = new Intent(this,final_Activity.class);
+            Bundle b = new Bundle();
+            b.putString("txt","You lose. Your Score is "+String.valueOf(counter));
+            //b.putString("finid","Better luck next time. Your score is "+String.valueOf(counter));
+            //Toast.makeText(this, "you lose, You're score = "+counter, Toast.LENGTH_SHORT).show();
+            j.putExtras(b);
+            startActivity(j);
+        }
+
+    }
+}
